b7992a67c3bbf56f04fc5b8ac72b5685a71a4310
MkGodh <fenryrvla@yandex.ru>
2023-12-11 19:22:26 +0300

    first commit

---
+++ Example_01/Program.cs
@@ -0,0 +1,21 @@
+﻿﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+
+
+int GetUserInput(string str)
+{
+    Console.WriteLine(str);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+bool DivisionTo(int num, int num1, int num2)
+{
+    return num%num1==0 && num % num2 ==0;
+}
+
+int number = GetUserInput("Enter number");
+bool div7a23 = DivisionTo(number,7,23);
+Console.WriteLine(div7a23 ? "да": "нет"); 
