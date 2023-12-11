d5c08e151cfcdd8a8cd2b23f60e512166786721c
MkGodh <fenryrvla@yandex.ru>
2023-12-11 19:37:03 +0300

    add second example

---
+++ Example_03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показ наибольшей цифр числа в промежутк 1, 99
+
+using System;
+
+class Program
+
+{
+static void Main()
+   {
+    Console.Write("Введите число из отреза [10, 99]: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    int firstDight = number / 10;
+    int secondDight = number % 10;
+
+    int maxDight = firstDight > secondDight ? firstDight : secondDight;
+    Console.WriteLine(maxDight);
+   }
+}
